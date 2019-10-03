4b2799b5c1b7bafdf4f2329be2a7b200c3cd6fe1
barabaiiika <pavel.rtnv@gmail.com>
2019-10-03 23:24:10 +0500

    feat: add task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,44 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+  if (str.length % 2) return false;
+  let pipeCounter = 0;
+
+  const bracketsHash = new Map();
+  const stack = [];
+
+  for (const subArray of bracketsConfig) {
+    if (subArray[0] === subArray[1]) {
+      bracketsHash.set(subArray[1],
+        {
+          isOpen: false,
+          value: subArray[0],
+          toString() {return this.value}
+        });
+    } else {
+      bracketsHash.set(subArray[1], subArray[0]);
+    }
+  }
+
+  for (const item of str) {
+    if (!bracketsHash.has(item)) {
+      stack.push(item);
+
+    } else if (bracketsHash.get(item).isOpen === false) {
+      stack.push(item);
+      bracketsHash.get(item).isOpen = true;
+
+    } else if (bracketsHash.get(item) == stack[stack.length - 1]) {
+      stack.pop();
+      if (bracketsHash.get(item).isOpen === true) { bracketsHash.get(item).isOpen = false };
+
+    } else {
+      return false;
+    }
+  }
+
+  if (stack.length === 0) {
+    return true;
+  } else {
+    return false;
+  }
 }
